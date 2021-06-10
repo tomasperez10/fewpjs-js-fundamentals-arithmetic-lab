34eb5c34375bd82a084cf4fcbf14b7407fc2302a
Tomas Perez <tomasitopg6@gmail.com>
2021-06-10 08:51:55 +0000

    done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+
+const random = Math.ceil(Math.random())
+
+let num3 = 16
+let num4 = 6
+const mod = num3 % num4
+
+const max = Math.max(5, 15, 20)
